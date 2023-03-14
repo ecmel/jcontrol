4482761a7d688e3f3e5eff79f187f6e1dd36eaa2
ecmel <ecmel.ercan@gmail.com>
2023-03-14 15:28:16 +0300

    first commit

---
+++ README.md
@@ -0,0 +1,59 @@
+# jControl
+
+Super simple JS controller.
+
+## Installation
+
+```bash
+npm install jcontrol --save-dev
+```
+
+## Application
+
+Application is the main class for bootstrapping. Controllers are registered on an application instance. For registering glob of controllers please refer to your bundler's documentation.
+
+```ts
+import { Application } from "jcontrol";
+import Search from "./controllers/search";
+
+const application = new Application();
+
+application.register("search", Search);
+
+application.run();
+```
+
+## Controllers
+
+Controllers are instances of classes that you register in your application. Each controller class inherits from the Controller base class.
+
+```html
+<div data-controller="search">
+    
+</div>
+```
+
+```ts
+import { Controller } from "jcontrol";
+
+export default class extends Controller {
+
+    connected() {
+        //
+    }
+
+    disconnected() {
+        //
+    }
+}
+```
+
+Controller classes are templated so more specific elements can be used if needed.
+
+```ts
+import { Controller } from "jcontrol";
+
+export default class extends Controller<HTMLElement> {
+    //
+}
+```
---
+++ index.ts
@@ -0,0 +1,108 @@
+export type Class<T> = new (...args: any[]) => T;
+
+export class Application {
+    #ctors = new Map<string, Class<Controller>>();
+    #controllers = new Map<Element, Controller>();
+    #observer: MutationObserver;
+
+    constructor() {
+        this.#observer = new MutationObserver((mutations) =>
+            this.#mutated(mutations)
+        );
+    }
+
+    #mutated(mutations: MutationRecord[]) {
+        mutations.forEach((mutation) => {
+            mutation.removedNodes.forEach((node) => this.#removeNode(node));
+            mutation.addedNodes.forEach((node) => this.#addNode(node));
+        });
+    }
+
+    #addNode(node: Node) {
+        if (node.nodeType === Node.ELEMENT_NODE) {
+            const el = node as Element;
+
+            if (el.hasAttribute("data-controller")) {
+                this.#addController(el);
+            }
+
+            el.querySelectorAll("[data-controller]").forEach((el) =>
+                this.#addController(el)
+            );
+        }
+    }
+
+    #removeNode(node: Node) {
+        if (node.nodeType === Node.ELEMENT_NODE) {
+            const el = node as Element;
+
+            el.querySelectorAll("[data-controller]").forEach((el) =>
+                this.#removeController(el)
+            );
+
+            if (el.hasAttribute("data-controller")) {
+                this.#removeController(el);
+            }
+        }
+    }
+
+    #addController(el: Element) {
+        const id = el.getAttribute("data-controller");
+        const ctor = this.#ctors.get(id);
+        const controller = new ctor(el, parent);
+
+        this.#controllers.set(el, controller);
+
+        queueMicrotask(() => controller.connected());
+    }
+
+    #removeController(el: Element) {
+        const controller = this.#controllers.get(el);
+
+        this.#controllers.delete(el);
+
+        queueMicrotask(() => controller.disconnected());
+    }
+
+    register(id: string, ctor: Class<Controller>) {
+        this.#ctors.set(id, ctor);
+    }
+
+    run() {
+        domReady().then(() => {
+            document
+                .querySelectorAll("[data-controller]")
+                .forEach((el) => this.#addController(el));
+
+            this.#observer.observe(document, {
+                childList: true,
+                subtree: true,
+            });
+        });
+    }
+}
+
+export class Controller<T extends Element = Element> {
+    #element: T;
+
+    constructor(element: T) {
+        this.#element = element;
+    }
+
+    get element() {
+        return this.#element;
+    }
+
+    connected() {}
+    disconnected() {}
+}
+
+function domReady() {
+    return new Promise<void>((resolve) => {
+        if (document.readyState == "loading") {
+            document.addEventListener("DOMContentLoaded", () => resolve());
+        } else {
+            resolve();
+        }
+    });
+}
